179835334155f3d8e741989b810e679e288c9b17
nazar2012 <133564811+nazar2012@users.noreply.github.com>
2026-01-11 11:46:50 +0200

    proj

--- src/app.js
+++ src/app.js
@@ -11,9 +11,54 @@ const input = document.querySelector(".data-input")
 const list = document.querySelector(".list")
 const sucess = document.querySelector(".sucess-box")
 
-input.addEventListener("input", debounce(searchCountry, 500))
+if (input) {
+    input.addEventListener("input", debounce(searchCountry, 500))
+}
 
 function searchCountry(event) {
-    console.log(event);
-    
+    const countryName = event.target.value.trim()
+    list.innerHTML = ""
+    sucess.innerHTML = ""
+
+    if (countryName === "") {
+        return
+    }
+    fetchCountries(countryName).then(res => {
+        if (res.length > 10) {
+            error({
+                text: 'Зробіть запит більш специфічним!',
+                delay: 1000
+            });
+            return
+        }
+        if (res.length > 1 && res.length <= 10) {
+            list.innerHTML = ""
+            sucess.innerHTML = ""
+            const country = res.map((item) => {
+                return `<li class="item">${item.name.common}</li>`
+            }).join("")
+            list.innerHTML = country
+        }
+        if (res.length === 1) {
+            list.innerHTML = ""
+            sucess.innerHTML = ""
+            const countryInfo = res.map((item) => {
+                const launguege = Object.values(item.languages)
+                return sucess.innerHTML = `
+                <h1 class="title">${item.name.common}</h1>
+            <div class="wrap">
+            <div class="box-one">
+            <h3 class="capital">Capital: <span>${item.capital}</span></h3>
+            <h3 class="population">Population: <span>${item.population}</span></h3>
+            <h4 class="launguege">Languages:</h4>
+            <ul class="list-laun">${launguege.map(item => {
+                    return `<li>${item}</li>`
+                }).join("")}</ul>
+            </div>
+            <img class="img" src="${item.flags.png}" alt="картинка">
+            </div>`
+            }).join("")
+        }
+        return
+    }).catch(error => console.log(error))
 }
--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,3 @@
 export default function fetchCountries(searchQuery) {
-    return fetch(`https://restcountries.com/v3.1/all?fields=name,flags?${searchQuery}`).then(res => res.json())
+    return fetch(`https://restcountries.com/v3.1/name/${searchQuery}`).then(res => res.json())
 }
